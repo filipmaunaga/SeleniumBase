2a79bcc6610555729cb60fab0257e2bd9b3f27ab
fico <fico@maher>
2019-12-16 20:43:46 +0100

    cas 31

---
+++ SeleniumBase/Cas31.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumBase
+{
+    class Cas31:SeleniumBaseClass
+    {
+        [Test]
+        public void Test1()
+        {
+            this.Navigate("https://www.toolsqa.com/automation-practice-form");
+            IWebElement firstName= this.FindElement(By.XPath("//input[@name='firstname']"));
+            this.SendKeys("Ime", false, firstName);
+            IWebElement lastName = this.FindElement(By.Id("lastname"));
+            this.SendKeys("Prezime", false, lastName);
+            DoWait(1);
+            //IWebElement gender = this.FindElement(By.Id("sex-0"));
+            //gender.Click();
+            //DoWait(1);
+            //IWebElement experience = this.FindElement(By.Id("exp-6"));
+            //experience.Click();
+            DoWait(1);
+            IWebElement datepick = this.FindElement(By.Id("datepicker"));
+            this.SendKeys(DateTime.Today.ToString(), false, datepick);
+            
+            DoWait(1);
+            IWebElement profession = this.FindElement(By.XPath("//input[@name='profession' and @value='Automation Tester']"));
+            profession.Click();
+            DoWait(1);
+            //IWebElement tool = this.FindElement(By.Id("tool-2"));
+            //tool.Click();
+            DoWait(1);
+            IWebElement continent = this.FindElement(By.Id("continents"));
+            var selection = new SelectElement(continent);
+            selection.SelectByText("Europe");
+            DoWait(2);
+
+
+
+
+
+
+
+
+        }
+        [SetUp]
+        public void SetUpTests()
+        {
+            this.Driver = new ChromeDriver();
+        }
+        [TearDown]
+        public void TearDownTests()
+        {
+            this.Close();
+        }
+    }
+}
